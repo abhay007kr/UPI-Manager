8889c5a37b6ce05ed2739e689642f92a28526a08
abhay007kr <aksabhay007@gmail.com>
2020-07-19 21:54:48 +0530

    RecyclerView Created with dummy data

--- app/src/main/java/com/example/upi_manager/MainActivity.kt
+++ app/src/main/java/com/example/upi_manager/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.upi_manager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var upiAdapter : UPIAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val upiRv = findViewById<RecyclerView>(R.id.rv)
+        upiRv.layoutManager = LinearLayoutManager(this,RecyclerView.VERTICAL,false)
+
+        val upi = ArrayList<UPI>()
+
+        upi.add(UPI("abhay","myself","500"))
+        upi.add(UPI("abhay","myself","500"))
+        upi.add(UPI("abhay","myself","500"))
+        upi.add(UPI("abhay","myself","500"))
+
+        val adapter = UPIAdapter(upi)
+
+        upiRv.adapter = adapter
+    }
+
+    fun initRecyclerView(){
+
     }
 }
